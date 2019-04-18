1a513df5a9204dd391bd8264aee0718b0f1e4d9a
aditya.jayanthy <aditya.jayanthy@172.29.67.14>
2019-04-18 10:25:16 +0530

    First

---
+++ Learn-Kafka/src/main/java/learn/kafka/KafkaConsumerDemo.java
@@ -0,0 +1,31 @@
+package learn.kafka;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class KafkaConsumerDemo {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "3.208.159.109:9092");
+		props.put("group.id", "test-grp-03");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.offset.reset", "earliest");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+		consumer.subscribe(Arrays.asList("my-kafka-topic2"));
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(50));
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+			}
+		}
+	}
+}
